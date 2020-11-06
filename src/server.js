0136bc40fd1bb6a5a42a6054c07d606709ffb13a
Dari Alves <darieltondf@gmail.com>
2020-11-06 16:26:11 -0300

    database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 const routes = require('./routes');
+require('dotenv/config');
 
+// Routes
 app.use(routes);
+
+// Connect to DATABASE
+const db = process.env.DATABASE;
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected!');
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-})
+});
