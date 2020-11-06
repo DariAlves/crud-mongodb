6bfaad2f1e8483eac7e362ea17907bc16833b3ce
Dari Alves <darieltondf@gmail.com>
2020-11-06 17:54:14 -0300

    updated crud

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,75 @@
 const { Router } = require('express');
-
 const usersRouter = Router();
+const User = require('../models/User');
 
-usersRouter.get('/', (request, response) => {
-    return response.status(200).json({ message: 'Hello world' })
+// ROUTES //
+// CRUD 
+
+// Create
+usersRouter.post('/', async (request, response) => {
+    try {
+        const { name, email } = request.body;
+        const user = new User({
+            name,
+            email
+        });
+
+        await user.save();
+
+        return response.json(user);
+    } catch (err) {
+        return response.json({ error: err })
+    }
 });
+
+// Read all
+usersRouter.get('/', async (request, response) => {
+    try {
+        const users = await User.find();
+
+        return response.json(users);
+    } catch (err) {
+        return response.json({ error: err })
+    }
+});
+
+// Read one by ID
+usersRouter.get('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const userById = await User.findById(id);
+
+        return response.json(userById);
+    } catch (err) {
+        return response.json({ error: err })
+    }
+});
+
+// Update
+usersRouter.put('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const { name, email } = request.body;
+
+        const updatedUser = await User.findByIdAndUpdate(id, { name, email });
+
+        return response.json(updatedUser);
+    } catch (err) {
+        return response.json({ error: err })
+    }
+});
+
+// Delete
+usersRouter.delete('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        return response.status(204).send();
+    } catch (err) {
+        return response.json({ error: err })
+    }
+})
 
 module.exports = usersRouter;
--- src/server.js
+++ src/server.js
@@ -3,15 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const routes = require('./routes');
-require('dotenv/config');
+require('dotenv').config();
 
 // Routes
+app.use(express.json());
 app.use(routes);
 
 // Connect to DATABASE
 const db = process.env.DATABASE;
 
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
     console.log('connected!');
 });
 
